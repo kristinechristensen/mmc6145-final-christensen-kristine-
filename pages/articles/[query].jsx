ac59b782562f8d5e7e5f297236beb8456fc11569
Kristine Christensen <christensen@morainevalley.edu>
2023-08-12 14:40:16 -0500

    update to remove API key

--- pages/articles/[query].jsx
+++ pages/articles/[query].jsx
@@ -25,7 +25,8 @@ export const getServerSideProps = withIronSessionSsr(
 
 
 export async function getServerSideProps({ params }) {
-  const NYT_API_KEY = "hsMsQYuhlWX070H87OV9DBZBRCRIiTUn";
+  const NYT_API_KEY = process.env.NYT_API_KEY;
+  
   const URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${params.query}&fq=section_name:("Travel")&api-key=${NYT_API_KEY}`;
 
   const results = await search(URL);
