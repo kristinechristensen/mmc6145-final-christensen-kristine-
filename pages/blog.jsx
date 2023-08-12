bd3d8a1f3df9aa0f3a93efc728c374aa0637eedc
Kristine Christensen <christensen@morainevalley.edu>
2023-08-12 16:45:42 -0500

    update

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -8,7 +8,7 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import useLogout from "../hooks/useLogout";
 import Hero from "../components/hero";
-import {TravelBlog} from "../db/models/blog";
+//import {TravelBlog} from "../db/models/blog";
 
 
 
